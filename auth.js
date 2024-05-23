a689a0b953a9dcffb4119818b79a5ef1b20e45ea
Ctheo <85225231+Christheoguipo@users.noreply.github.com>
2024-05-23 23:04:34 +0200

    Replaced secret key with a much harder to guess key.

--- auth.js
+++ auth.js
@@ -1,27 +1,26 @@
-const jwtSecret = 'your_jwt_secret'; // This has to be the same key used in the JWTStrategy
+const jwtSecret = process.env.JWT_SECRET; // This has to be the same key used in the JWTStrategy
 
-const jwt = require('jsonwebtoken'),
-  passport = require('passport');
+const jwt = require("jsonwebtoken"),
+  passport = require("passport");
 
-require('./passport'); // Local passport file
+require("./passport"); // Local passport file
 
 let generateJWTToken = (user) => {
   return jwt.sign(user, jwtSecret, {
     subject: user.Username, // This is the username that's being encoded in the JWT
-    expiresIn: '7d', // Specifies that the token will expire in / days
-    algorithm: 'HS256' // The algorithm used to sign or encode the values to the JWT 
+    expiresIn: "7d", // Specifies that the token will expire in / days
+    algorithm: "HS256", // The algorithm used to sign or encode the values to the JWT
   });
-}
+};
 
 module.exports = (router) => {
-
-  router.post('/login', (req, res) => {
-    passport.authenticate('local', { session: false }, (error, user, info) => {
+  router.post("/login", (req, res) => {
+    passport.authenticate("local", { session: false }, (error, user, info) => {
       if (error || !user) {
         return res.status(400).json({
           // message: 'Something is not right.',
-          message: 'Invalid login. Please check your Username or Password.',
-          user: user
+          message: "Invalid login. Please check your Username or Password.",
+          user: user,
         });
       }
       req.login(user, { session: false }, (error) => {
@@ -33,4 +32,4 @@ module.exports = (router) => {
       });
     })(req, res);
   });
-}
+};
